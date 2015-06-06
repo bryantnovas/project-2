13d1f09f0bb413208d4d6f9f3360b00a5f2dae62
Bryant Novas <bryant.novas@gmail.com>
2015-06-06 09:57:25 -0400

    6/6/15

--- forum.rb
+++ forum.rb
@@ -10,14 +10,17 @@ module App
 			erb :welcome
 		end
 		post '/posts' do 
-			@topics = params['topic']
+			topic = params['topic']
 			query = "INSERT INTO posts (topic) VALUES ($1)"
-			$db.exec_params(query, [@topics])
-			# put data in the database
+			$db.exec_params(query, [topic])
+			# put data in the database]
+			name = params['name']
+			query1 = "INSERT INTO users (name) VALUES ($1)"
+			$db.exec_params(query1, [name])
 			redirect '/posts'
 		end
 		get '/posts' do
-			query = "SELECT * FROM posts "
+			query = "SELECT *,users.name FROM posts JOIN users ON posts.id = users.id "
 			@posts = $db.exec(query)
 			erb :posts
 			# get all the posts from the database
